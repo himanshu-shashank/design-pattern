edcaf82c19ead0b7cee961cbccefd7f9e94e8ca3
Himanshu Shashank <himanshu.shashank@oracle.com>
2020-11-10 15:20:36 +0530

    cache proxy

---
+++ src/main/java/com/himanshu/designpattern/structural/proxy/cachingproxy/Demo.java
@@ -0,0 +1,36 @@
+package com.himanshu.designpattern.structural.proxy.cachingproxy;
+
+import com.himanshu.designpattern.structural.proxy.cachingproxy.downloader.YouTubeDownloader;
+import com.himanshu.designpattern.structural.proxy.cachingproxy.proxy.YouTubeCacheProxy;
+import com.himanshu.designpattern.structural.proxy.cachingproxy.some_cool_media_library.ThirdPartyYouTubeClass;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        YouTubeDownloader naiveDownloader = new YouTubeDownloader(new ThirdPartyYouTubeClass());
+        YouTubeDownloader smartDownloader = new YouTubeDownloader(new YouTubeCacheProxy());
+
+        long naive = test(naiveDownloader);
+        long smart = test(smartDownloader);
+        System.out.print("Time saved by caching proxy: " + (naive - smart) + "ms");
+
+    }
+
+    private static long test(YouTubeDownloader downloader) {
+        long startTime = System.currentTimeMillis();
+
+        // User behavior in our app:
+        downloader.renderPopularVideos();
+        downloader.renderVideoPage("catzzzzzzzzz");
+        downloader.renderPopularVideos();
+        downloader.renderVideoPage("dancesvideoo");
+        // Users might visit the same page quite often.
+        downloader.renderVideoPage("catzzzzzzzzz");
+        downloader.renderVideoPage("someothervid");
+
+        long estimatedTime = System.currentTimeMillis() - startTime;
+        System.out.print("Time elapsed: " + estimatedTime + "ms\n");
+        return estimatedTime;
+    }
+}
+
