4b8deb2ee391c287c285fa056fd8962707b39e9e
Himanshu Shashank <himanshu.shashank@oracle.com>
2020-11-10 15:10:06 +0530

    facade

---
+++ src/main/java/com/himanshu/designpattern/structural/facade/some_complex_media_library/Codec.java
@@ -0,0 +1,4 @@
+package com.himanshu.designpattern.structural.facade.some_complex_media_library;
+
+public interface Codec {
+}
---
+++ src/main/java/com/himanshu/designpattern/structural/facade/some_complex_media_library/OggCompressionCodec.java
@@ -0,0 +1,5 @@
+package com.himanshu.designpattern.structural.facade.some_complex_media_library;
+
+public class OggCompressionCodec implements Codec {
+    public String type = "ogg";
+}
