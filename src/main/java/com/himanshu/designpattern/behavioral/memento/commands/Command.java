4aa1f1a9a45f0f376f164ad4aedd96a528b73b44
Himanshu Shashank <himanshu.shashank@oracle.com>
2020-11-18 21:11:23 +0530

    memento

---
+++ src/main/java/com/himanshu/designpattern/behavioral/memento/commands/Command.java
@@ -0,0 +1,6 @@
+package com.himanshu.designpattern.behavioral.memento.commands;
+
+public interface Command {
+    String getName();
+    void execute();
+}
---
+++ src/main/java/com/himanshu/designpattern/behavioral/memento/history/Memento.java
@@ -0,0 +1,17 @@
+package com.himanshu.designpattern.behavioral.memento.history;
+
+import com.himanshu.designpattern.behavioral.memento.editor.Editor;
+
+public class Memento {
+    private String backup;
+    private Editor editor;
+
+    public Memento(Editor editor) {
+        this.editor = editor;
+        this.backup = editor.backup();
+    }
+
+    public void restore() {
+        editor.restore(backup);
+    }
+}
