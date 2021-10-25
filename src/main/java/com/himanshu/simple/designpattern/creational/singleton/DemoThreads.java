66ed6fa4a82f8ac1200fd9d3095d93e112039080
Himanshu Shashank <himanshu.shashank@oracle.com>
2021-10-25 23:04:36 +0530

    singleton with scrabble game example

---
+++ src/main/java/com/himanshu/simple/designpattern/creational/singleton/DemoThreads.java
@@ -0,0 +1,19 @@
+package com.himanshu.simple.designpattern.creational.singleton;
+
+public class DemoThreads {
+    public static void main(String[] args){
+
+        // Create a new Thread created using the Runnable interface
+        // Execute the code in run after 10 seconds
+
+        Runnable getTiles = new GetTheTiles();
+
+        Runnable getTilesAgain = new GetTheTiles();
+
+        // Call for the code in the method run to execute
+
+        new Thread(getTiles).start();
+        new Thread(getTilesAgain).start();
+
+    }
+}
