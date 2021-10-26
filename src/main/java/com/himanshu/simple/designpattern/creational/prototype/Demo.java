058fdf9b5a29d96d2a32f85b8f7ba916a808f72c
Himanshu Shashank <himanshu.shashank@oracle.com>
2021-10-26 16:39:24 +0530

    prototype creational design pattern

---
+++ src/main/java/com/himanshu/simple/designpattern/creational/prototype/Demo.java
@@ -0,0 +1,33 @@
+package com.himanshu.simple.designpattern.creational.prototype;
+
+import com.himanshu.simple.designpattern.creational.prototype.animal.Animal;
+import com.himanshu.simple.designpattern.creational.prototype.animal.Sheep;
+
+public class Demo {
+    public static void main(String[] args){
+
+        // Handles routing makeCopy method calls to the
+        // right subclasses of Animal
+
+        CloneFactory animalMaker = new CloneFactory();
+
+        // Creates a new Sheep instance
+
+        Sheep sally = new Sheep();
+
+        // Creates a clone of Sally and stores it in its own
+        // memory location
+
+        Sheep clonedSheep = (Sheep)animalMaker.getClone(sally);
+
+        // These are exact copies of each other
+
+        System.out.println(sally);
+
+        System.out.println(clonedSheep);
+
+        System.out.println("Sally HashCode: " + System.identityHashCode(System.identityHashCode(sally)));
+
+        System.out.println("Clone HashCode: " + System.identityHashCode(System.identityHashCode(clonedSheep)));
+    }
+}
