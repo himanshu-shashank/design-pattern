5dc18d64a15753d7b2981bd85412a1dc84cc91a5
Himanshu Shashank <himanshu.shashank@oracle.com>
2021-10-29 20:18:47 +0530

    composite pattern

---
+++ src/main/java/com/himanshu/simple/designpattern/structural/composite/DiscJockey.java
@@ -0,0 +1,27 @@
+package com.himanshu.simple.designpattern.structural.composite;
+
+import com.himanshu.simple.designpattern.structural.composite.songs.SongComponent;
+
+public class DiscJockey{
+
+    SongComponent songList;
+
+    // newSongList contains every Song, SongGroup,
+    // and any Songs saved in SongGroups
+
+    public DiscJockey(SongComponent newSongList){
+
+        songList = newSongList;
+
+    }
+
+    // Calls the displaySongInfo() on every Song
+    // or SongGroup stored in songList
+
+    public void getSongList(){
+
+        songList.displaySongInfo();
+
+    }
+
+}
